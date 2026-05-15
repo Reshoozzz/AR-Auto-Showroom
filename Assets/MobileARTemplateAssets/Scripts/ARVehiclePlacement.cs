6273108667405992a197b05c28cb39e02ce8bf95
Reshoozzz <kareemresha901@gmail.com>
2026-05-15 23:09:24 +0300

    Added plane hiding after vehicle placement

--- Assets/MobileARTemplateAssets/Scripts/ARVehiclePlacement.cs
+++ Assets/MobileARTemplateAssets/Scripts/ARVehiclePlacement.cs
@@ -8,12 +8,14 @@ public class ARVehiclePlacement : MonoBehaviour
 {
     [Header("AR Components")]
     public ARRaycastManager raycastManager;
+    public ARPlaneManager planeManager;
 
     [Header("Vehicle Prefab")]
     public GameObject vehiclePrefab;
 
     [Header("Placement Settings")]
     public bool allowMultiplePlacement = false;
+    public bool hidePlanesAfterPlacement = true;
 
     [Header("Rotation Settings")]
     public float rotationSpeed = 0.2f;
@@ -76,22 +78,19 @@ public class ARVehiclePlacement : MonoBehaviour
 
             if (spawnedVehicle == null)
             {
-                spawnedVehicle = Instantiate(
-                    vehiclePrefab,
-                    hitPose.position,
-                    rotation
-                );
+                spawnedVehicle = Instantiate(vehiclePrefab, hitPose.position, rotation);
 
                 targetYRotation = spawnedVehicle.transform.eulerAngles.y;
                 targetScale = spawnedVehicle.transform.localScale;
+
+                if (hidePlanesAfterPlacement)
+                {
+                    SetPlaneVisibility(false);
+                }
             }
             else
             {
-                spawnedVehicle.transform.SetPositionAndRotation(
-                    hitPose.position,
-                    rotation
-                );
-
+                spawnedVehicle.transform.SetPositionAndRotation(hitPose.position, rotation);
                 targetYRotation = spawnedVehicle.transform.eulerAngles.y;
             }
         }
@@ -138,40 +137,21 @@ public class ARVehiclePlacement : MonoBehaviour
         Vector2 touch1PreviousPosition = touch1.position - touch1.deltaPosition;
         Vector2 touch2PreviousPosition = touch2.position - touch2.deltaPosition;
 
-        float previousDistance = Vector2.Distance(
-            touch1PreviousPosition,
-            touch2PreviousPosition
-        );
-
-        float currentDistance = Vector2.Distance(
-            touch1.position,
-            touch2.position
-        );
+        float previousDistance = Vector2.Distance(touch1PreviousPosition, touch2PreviousPosition);
+        float currentDistance = Vector2.Distance(touch1.position, touch2.position);
 
         float distanceDifference = currentDistance - previousDistance;
 
         Vector3 newScale = targetScale + Vector3.one * distanceDifference * scaleSpeed;
 
-        float clampedScale = Mathf.Clamp(
-            newScale.x,
-            minimumScale,
-            maximumScale
-        );
+        float clampedScale = Mathf.Clamp(newScale.x, minimumScale, maximumScale);
 
-        targetScale = new Vector3(
-            clampedScale,
-            clampedScale,
-            clampedScale
-        );
+        targetScale = new Vector3(clampedScale, clampedScale, clampedScale);
     }
 
     private void SmoothTransformUpdates()
     {
-        Quaternion targetRotation = Quaternion.Euler(
-            0,
-            targetYRotation,
-            0
-        );
+        Quaternion targetRotation = Quaternion.Euler(0, targetYRotation, 0);
 
         spawnedVehicle.transform.rotation = Quaternion.Lerp(
             spawnedVehicle.transform.rotation,
@@ -184,6 +164,19 @@ public class ARVehiclePlacement : MonoBehaviour
             targetScale,
             Time.deltaTime * scaleSmoothness
         );
+    }
+
+    private void SetPlaneVisibility(bool isVisible)
+    {
+        if (planeManager == null)
+            return;
+
+        foreach (ARPlane plane in planeManager.trackables)
+        {
+            plane.gameObject.SetActive(isVisible);
+        }
+
+        planeManager.enabled = isVisible;
     }
 
     private bool IsPointerOverUI(Touch touch)
@@ -200,6 +193,7 @@ public class ARVehiclePlacement : MonoBehaviour
         {
             Destroy(spawnedVehicle);
             spawnedVehicle = null;
+            SetPlaneVisibility(true);
         }
     }
 }
